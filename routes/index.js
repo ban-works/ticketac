13e86f45be2a1d063c7e5f56cf40817eef064cbb
Alban Fresil <alban.fresil@gmail.com>
2022-06-30 16:45:26 +0200

    journey model + route results

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-
+var JourneyModel = require('../models/journey.js');
 const mongoose = require("mongoose");
 
 // useNewUrlParser ;)
@@ -55,25 +55,35 @@ var date = [
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index");
 });
 // GET booking
 router.get("/booking", function (req, res, next) {
-  res.render("booking", { title: "Express" });
+  res.render("booking");
 });
 // GET results
-router.get("/results", function (req, res, next) {
-  res.render("results", { title: "Express" });
+router.post("/results", async function (req, res, next) {
+  var departure = req.body.departure;
+  var arrival = req.body.arrival;
+  var date = req.body.date;
+  journeys = await JourneyModel.find({ departure: departure, arrival: arrival, date: date});
+  console.log(journeys)
+  if(journeys.length<1){
+    res.redirect('/notfound')
+  }else{
+    res.render("results", {journeys});
+
+  }
 });
 
 router.get("/notfound", function (req, res, next) {
-  res.render("notfound", { title: "Express" });
+  res.render("notfound", );
 });
 router.get("/lasttrips", function (req, res, next) {
-  res.render("lasttrips", { title: "Express" });
+  res.render("lasttrips", );
 });
 router.get("/my-tickets", function (req, res, next) {
-  res.render("my-tickets", { title: "Express" });
+  res.render("my-tickets",);
 });
 
 // Remplissage de la base de donnée, une fois suffit
@@ -103,22 +113,22 @@ router.get("/save", async function (req, res, next) {
 
 // Cette route est juste une verification du Save.
 // Vous pouvez choisir de la garder ou la supprimer.
-router.get("/result", function (req, res, next) {
-  // Permet de savoir combien de trajets il y a par ville en base
-  for (i = 0; i < city.length; i++) {
-    journeyModel.find(
-      { departure: city[i] }, //filtre
+// router.get("/result", function (req, res, next) {
+//   // Permet de savoir combien de trajets il y a par ville en base
+//   for (i = 0; i < city.length; i++) {
+//     journeyModel.find(
+//       { departure: city[i] }, //filtre
 
-      function (err, journey) {
-        console.log(
-          `Nombre de trajets au départ de ${journey[0].departure} : `,
-          journey.length
-        );
-      }
-    );
-  }
+//       function (err, journey) {
+//         console.log(
+//           `Nombre de trajets au départ de ${journey[0].departure} : `,
+//           journey.length
+//         );
+//       }
+//     );
+//   }
 
-  res.render("index");
-});
+//   res.render("index");
+// });
 
 module.exports = router;
