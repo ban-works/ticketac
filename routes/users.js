cbe1aee0eb04981a821832e3381d28ad0e9e0c13
Alban Fresil <alban.fresil@gmail.com>
2022-07-01 13:08:35 +0200

    prout2

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 var express = require('express');
-const { updateOne } = require('../models/journey');
+var JourneyModel = require('../models/journey');
 var router = express.Router();
 var UserModel = require('../models/users');
 
